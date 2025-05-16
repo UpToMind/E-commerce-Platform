f38273d90650e1a6e75e4b162f31e6db5c51ee08
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-05-16 17:16:54 +0900

    feature[order-domain-core] : add OrderDomainService Interface

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/ecommerce/system/order/service/domain/OrderDomainService.java
@@ -0,0 +1,21 @@
+package com.ecommerce.system.order.service.domain;
+
+import com.ecommerce.system.order.service.domain.entity.Order;
+import com.ecommerce.system.order.service.domain.entity.Seller;
+import com.ecommerce.system.order.service.domain.event.OrderCancelledEvent;
+import com.ecommerce.system.order.service.domain.event.OrderCreatedEvent;
+import com.ecommerce.system.order.service.domain.event.OrderPaidEvent;
+
+import java.util.List;
+
+public interface OrderDomainService {
+    OrderCreatedEvent validateAndInitiateOrder(Order order, Seller seller);
+
+    OrderPaidEvent payOrder(Order order);
+
+    void approveOrder(Order order);
+
+    OrderCancelledEvent cancelOrderPayment(Order order, List<String> failureMessages);
+
+    void cancelOrder(Order order, List<String> failureMessages);
+}
