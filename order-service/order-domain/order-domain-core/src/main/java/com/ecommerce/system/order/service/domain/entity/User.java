579370a23780c437862da928108511e4f685ae84
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-05-27 18:07:17 +0900

    fix[domain-core] : fix User entity

--- order-service/order-domain/order-domain-core/src/main/java/com/ecommerce/system/order/service/domain/entity/User.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/ecommerce/system/order/service/domain/entity/User.java
@@ -4,5 +4,30 @@ import com.ecommerce.system.domain.entity.AggregateRoot;
 import com.ecommerce.system.domain.valueobject.UserId;
 
 public class User extends AggregateRoot<UserId> {
+    private String username;
+    private String firstName;
+    private String lastName;
 
+    public User(UserId userId, String username, String firstName, String lastName) {
+        super.setId(userId);
+        this.username = username;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(UserId userId) {
+        super.setId(userId);
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
 }
