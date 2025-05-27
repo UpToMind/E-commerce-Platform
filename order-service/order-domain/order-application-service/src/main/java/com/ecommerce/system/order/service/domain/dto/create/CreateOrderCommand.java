0b0fd44cff175e91099d815080e17b83a6191a8f
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-05-27 17:49:59 +0900

    feature[order-application-service] : add CreateOrderCommand dto

---
+++ order-service/order-domain/order-application-service/src/main/java/com/ecommerce/system/order/service/domain/dto/create/CreateOrderCommand.java
@@ -0,0 +1,26 @@
+package com.ecommerce.system.order.service.domain.dto.create;
+
+import com.ecommerce.system.order.service.domain.entity.OrderItem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NonNull;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CreateOrderCommand {
+
+    @NonNull
+    private final UUID customerId;
+    @NonNull
+    private final UUID sellerId;
+    @NonNull
+    private final BigDecimal price;
+    @NonNull
+    private final List<OrderItem> items;
+}
