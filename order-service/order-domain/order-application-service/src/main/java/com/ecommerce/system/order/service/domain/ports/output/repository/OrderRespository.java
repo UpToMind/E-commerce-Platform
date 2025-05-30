0af19caa45381dbc5dcc996ea97ec8190b26c247
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-05-30 17:50:39 +0900

    feature[output.repository] : add OrderRepository

---
+++ order-service/order-domain/order-application-service/src/main/java/com/ecommerce/system/order/service/domain/ports/output/repository/OrderRespository.java
@@ -0,0 +1,16 @@
+package com.ecommerce.system.order.service.domain.ports.output.repository;
+
+import com.ecommerce.system.domain.valueobject.OrderId;
+import com.ecommerce.system.order.service.domain.entity.Order;
+import com.ecommerce.system.order.service.domain.valueobject.TrackingId;
+
+import java.util.Optional;
+
+public interface OrderRespository {
+
+    Order save(Order order);
+
+    Optional<Order> findById(OrderId orderId);
+
+    Optional<Order> findByTrackingId(TrackingId trackingId);
+}
