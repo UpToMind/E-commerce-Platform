e17c4dcac54d32d7215fcf3a58b62e74602a62bc
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-05-31 16:45:38 +0900

    feature[output.repository] : add UserRepository

---
+++ order-service/order-domain/order-application-service/src/main/java/com/ecommerce/system/order/service/domain/ports/output/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ecommerce.system.order.service.domain.ports.output.repository;
+
+import com.ecommerce.system.order.service.domain.entity.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository {
+    Optional<User> findUser(UUID userId);
+
+    User save(User user);
+}
