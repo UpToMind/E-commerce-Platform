d6cb1e456009de2249d9a590bbef6a891613bae1
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-02 09:43:49 +0900

    feature[order-application-service] : add OrderCreatedCommandHandler, OrderTrackCommandHandler

---
+++ order-service/order-domain/order-application-service/src/main/java/com/ecommerce/system/order/service/domain/OrderTrackCommandHandler.java
@@ -0,0 +1,10 @@
+package com.ecommerce.system.order.service.domain;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OrderTrackCommandHandler {
+
+}
