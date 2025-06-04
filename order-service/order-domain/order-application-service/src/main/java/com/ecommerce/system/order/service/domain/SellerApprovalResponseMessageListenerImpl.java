61b5e4c19e06ff326fd010005efa2a5a00ad8e82
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-04 10:29:59 +0900

    fix[order-application-service] : add annotation and import

--- order-service/order-domain/order-application-service/src/main/java/com/ecommerce/system/order/service/domain/SellerApprovalResponseMessageListenerImpl.java
+++ order-service/order-domain/order-application-service/src/main/java/com/ecommerce/system/order/service/domain/SellerApprovalResponseMessageListenerImpl.java
@@ -1,5 +1,6 @@
 package com.ecommerce.system.order.service.domain;
 
+import com.ecommerce.system.order.service.domain.dto.message.SellerApprovalResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
