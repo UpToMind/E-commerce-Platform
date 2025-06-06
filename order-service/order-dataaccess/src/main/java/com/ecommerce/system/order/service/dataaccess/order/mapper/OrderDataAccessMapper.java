d3a9b9dccc704ae6e07aa20663f3546f104fa888
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-06 16:30:45 +0900

    feature[order-dataaccess] : add OrderDataAccessMapper

---
+++ order-service/order-dataaccess/src/main/java/com/ecommerce/system/order/service/dataaccess/order/mapper/OrderDataAccessMapper.java
@@ -0,0 +1,76 @@
+package com.ecommerce.system.order.service.dataaccess.order.mapper;
+
+import com.ecommerce.system.domain.valueobject.*;
+import com.ecommerce.system.order.service.dataaccess.order.entity.OrderEntity;
+import com.ecommerce.system.order.service.dataaccess.order.entity.OrderItemEntity;
+import com.ecommerce.system.order.service.domain.entity.Order;
+import com.ecommerce.system.order.service.domain.entity.OrderItem;
+import com.ecommerce.system.order.service.domain.entity.Product;
+import com.ecommerce.system.order.service.domain.valueobject.OrderItemId;
+import com.ecommerce.system.order.service.domain.valueobject.TrackingId;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class OrderDataAccessMapper {
+    public OrderEntity orderToOrderEntity(Order order) {
+        OrderEntity orderEntity = OrderEntity.builder()
+                .id(order.getId().getValue())
+                .userId(order.getUserId().getValue())
+                .sellerId(order.getSellerId().getValue())
+                .trackingId(order.getTrackingId().getValue())
+                .price(order.getPrice().getAmount())
+                .items(orderItemsToOrderItemEntities(order.getItems()))
+                .orderStatus(order.getOrderStatus())
+                .failureMessages(order.getFailureMessages() != null ?
+                        String.join(FAILURE_MESSAGE_DELIMITER, order.getFailureMessages()) : "")
+                .build();
+        orderEntity.getItems().forEach(orderItemEntity -> orderItemEntity.setOrder(orderEntity));
+
+        return orderEntity;
+    }
+
+    public Order orderEntityToOrder(OrderEntity orderEntity) {
+        return Order.builder()
+                .orderId(new OrderId(orderEntity.getId()))
+                .userId(new UserId(orderEntity.getUserId()))
+                .sellerId(new SellerId(orderEntity.getSellerId()))
+                .price(new Money(orderEntity.getPrice()))
+                .items(orderItemEntitiesToOrderItems(orderEntity.getItems()))
+                .trackingId(new TrackingId(orderEntity.getTrackingId()))
+                .orderStatus(orderEntity.getOrderStatus())
+                .failureMessages(orderEntity.getFailureMessages().isEmpty() ? new ArrayList<>() :
+                        new ArrayList<>(Arrays.asList(orderEntity.getFailureMessages()
+                                .split(FAILURE_MESSAGE_DELIMITER))))
+                .build();
+    }
+
+    private List<OrderItem> orderItemEntitiesToOrderItems(List<OrderItemEntity> items) {
+        return items.stream()
+                .map(orderItemEntity -> OrderItem.builder()
+                        .orderItemId(new OrderItemId(orderItemEntity.getId()))
+                        .product(new Product(new ProductId(orderItemEntity.getProductId())))
+                        .price(new Money(orderItemEntity.getPrice()))
+                        .quantity(orderItemEntity.getQuantity())
+                        .subTotal(new Money(orderItemEntity.getSubTotal()))
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+    private List<OrderItemEntity> orderItemsToOrderItemEntities(List<OrderItem> items) {
+        return items.stream()
+                .map(orderItem -> OrderItemEntity.builder()
+                        .id(orderItem.getId().getValue())
+                        .productId(orderItem.getProduct().getId().getValue())
+                        .price(orderItem.getPrice().getAmount())
+                        .quantity(orderItem.getQuantity())
+                        .subTotal(orderItem.getSubTotal().getAmount())
+                        .build())
+                .collect(Collectors.toList());
+    }
+
+}
