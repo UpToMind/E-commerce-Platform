d5c8e75df5e8a9d39264777f2d2d0d395fbc3384
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-08 10:17:21 +0900

    feature[user.adapter] : completing UserRepositoryImpl

--- order-service/order-dataaccess/src/main/java/com/ecommerce/system/order/service/dataaccess/user/adapter/UserRepositoryImpl.java
+++ order-service/order-dataaccess/src/main/java/com/ecommerce/system/order/service/dataaccess/user/adapter/UserRepositoryImpl.java
@@ -1,4 +1,27 @@
 package com.ecommerce.system.order.service.dataaccess.user.adapter;
 
-public class UserRepositoryImpl {
+import com.ecommerce.system.order.service.dataaccess.user.mapper.UserDataAccessMapper;
+import com.ecommerce.system.order.service.dataaccess.user.repository.UserJpaRepository;
+import com.ecommerce.system.order.service.domain.entity.User;
+import com.ecommerce.system.order.service.domain.ports.output.repository.UserRepository;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Component
+public class UserRepositoryImpl implements UserRepository {
+    private final UserJpaRepository userJpaRepository;
+    private final UserDataAccessMapper userDataAccessMapper;
+
+    public UserRepositoryImpl(UserJpaRepository userJpaRepository,
+                              UserDataAccessMapper userDataAccessMapper) {
+        this.userJpaRepository = userJpaRepository;
+        this.userDataAccessMapper = userDataAccessMapper;
+    }
+
+    @Override
+    public Optional<User> findUser(UUID userId) {
+        return userJpaRepository.findById(userId).map(userDataAccessMapper::userEntityToUser);
+    }
 }
