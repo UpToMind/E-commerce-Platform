5155c00e6aad8f25c0fc0e6d0994259afc895e3b
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-09 13:23:22 +0900

    chore[order-messaging] : delete useless import

--- order-service/order-messaging/src/main/java/com/ecommerce/system/order/service/messaging/publisher/kafka/CreateOrderKafkaMessagePublisher.java
+++ order-service/order-messaging/src/main/java/com/ecommerce/system/order/service/messaging/publisher/kafka/CreateOrderKafkaMessagePublisher.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.producer.RecordMetadata;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
-import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.function.BiConsumer;
 
