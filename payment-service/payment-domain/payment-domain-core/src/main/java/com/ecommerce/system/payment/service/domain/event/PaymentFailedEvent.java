ec1f3a9e9bac4d396cfc217b942c2547e3d5ecb7
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-16 14:26:24 +0900

    feature[domain-core] : add payment related events

---
+++ payment-service/payment-domain/payment-domain-core/src/main/java/com/ecommerce/system/payment/service/domain/event/PaymentFailedEvent.java
@@ -0,0 +1,16 @@
+package com.ecommerce.system.payment.service.domain.event;
+
+import com.ecommerce.system.payment.service.domain.entity.Payment;
+
+import java.time.ZonedDateTime;
+import java.util.List;
+
+public class PaymentFailedEvent extends PaymentEvent {
+
+    public PaymentFailedEvent(Payment payment,
+                              ZonedDateTime createdAt,
+                              List<String> failureMessages) {
+        super(payment, createdAt, failureMessages);
+    }
+
+}
