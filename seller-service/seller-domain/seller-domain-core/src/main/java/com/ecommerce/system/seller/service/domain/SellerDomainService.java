835d6c5ee082c82cfad011d27fcc2fbe63cbc5a2
UpToMind <114003042+UpToMind@users.noreply.github.com>
2025-06-19 13:53:23 +0900

    feature[seller-domain-core] : complete SellerDomainService

--- seller-service/seller-domain/seller-domain-core/src/main/java/com/ecommerce/system/seller/service/domain/SellerDomainService.java
+++ seller-service/seller-domain/seller-domain-core/src/main/java/com/ecommerce/system/seller/service/domain/SellerDomainService.java
@@ -1,4 +1,17 @@
 package com.ecommerce.system.seller.service.domain;
 
+import com.ecommerce.system.domain.event.publisher.DomainEventPublisher;
+import com.ecommerce.system.seller.service.domain.entity.Seller;
+import com.ecommerce.system.seller.service.domain.event.OrderApprovalEvent;
+import com.ecommerce.system.seller.service.domain.event.OrderApprovedEvent;
+import com.ecommerce.system.seller.service.domain.event.OrderRejectedEvent;
+
+import java.util.List;
+
 public interface SellerDomainService {
+
+    OrderApprovalEvent validateOrder(Seller seller,
+                                     List<String> failureMessages,
+                                     DomainEventPublisher<OrderApprovedEvent> orderApprovedEventDomainEventPublisher,
+                                     DomainEventPublisher<OrderRejectedEvent> orderRejectedEventDomainEventPublisher);
 }
--- seller-service/seller-domain/seller-domain-core/src/main/java/com/ecommerce/system/seller/service/domain/SellerDomainServiceImpl.java
+++ seller-service/seller-domain/seller-domain-core/src/main/java/com/ecommerce/system/seller/service/domain/SellerDomainServiceImpl.java
@@ -1,4 +1,48 @@
 package com.ecommerce.system.seller.service.domain;
 
+
+import com.ecommerce.system.domain.event.publisher.DomainEventPublisher;
+import com.ecommerce.system.domain.valueobject.OrderApprovalStatus;
+import com.ecommerce.system.seller.service.domain.entity.Seller;
+import com.ecommerce.system.seller.service.domain.event.OrderApprovalEvent;
+import com.ecommerce.system.seller.service.domain.event.OrderApprovedEvent;
+import com.ecommerce.system.seller.service.domain.event.OrderRejectedEvent;
+import lombok.extern.slf4j.Slf4j;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+import static com.ecommerce.system.domain.DomainConstants.UTC;
+
+@Slf4j
 public class SellerDomainServiceImpl implements SellerDomainService{
+    @Override
+    public OrderApprovalEvent validateOrder(Seller seller,
+                                            List<String> failureMessages,
+                                            DomainEventPublisher<OrderApprovedEvent>
+                                                    orderApprovedEventDomainEventPublisher,
+                                            DomainEventPublisher<OrderRejectedEvent>
+                                                    orderRejectedEventDomainEventPublisher) {
+        seller.validateOrder(failureMessages);
+        log.info("Validating order with id: {}", seller.getOrderDetail().getId().getValue());
+
+        if (failureMessages.isEmpty()) {
+            log.info("Order is approved for order id: {}", seller.getOrderDetail().getId().getValue());
+            seller.constructOrderApproval(OrderApprovalStatus.APPROVED);
+            return new OrderApprovedEvent(seller.getOrderApproval(),
+                    seller.getId(),
+                    failureMessages,
+                    ZonedDateTime.now(ZoneId.of(UTC)),
+                    orderApprovedEventDomainEventPublisher);
+        } else {
+            log.info("Order is rejected for order id: {}", seller.getOrderDetail().getId().getValue());
+            seller.constructOrderApproval(OrderApprovalStatus.REJECTED);
+            return new OrderRejectedEvent(seller.getOrderApproval(),
+                    seller.getId(),
+                    failureMessages,
+                    ZonedDateTime.now(ZoneId.of(UTC)),
+                    orderRejectedEventDomainEventPublisher);
+        }
+    }
 }
